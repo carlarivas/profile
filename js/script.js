5b747a63c2df6325ec7c20219034b72c3753b6b6
Carla Rivas <rivas.sarinana@outlook.com>
2023-02-07 01:53:48 -0600

    A

--- js/script.js
+++ js/script.js
@@ -31,32 +31,25 @@ new Waypoint({
 	offset: 'bottom-in-view',
 });
 
-/* Make navbar fixed when scrolled down and hide it when scrolled up */
-const navbar = $('.navbar-top');
-let previousScroll = 0;
-$(window).on('scroll', function (event) {
-	let currentScroll = $(window).scrollTop(); //Distance scrolled down the page
-	let navHeight = $(navbar).height(); //Height of navbar
-	if (currentScroll > 10) {
-		$('.navbar-top').addClass('navbar-top--scrolled');
-	} else {
-		$('.navbar-top').removeClass('navbar-top--scrolled');
-	}
+$(document).ready(function(){	
+	$(window).bind('scroll', function() {
+	var distance = 25;
+		  if ($(window).scrollTop() > $(window).height()-75) {
+			  $('nav').fadeOut(500);
+		  }
+		  else {
+			  $('nav').fadeIn(500);
+		  }
 
-	//When scrolling down AND you've scrolled past navHeight * 2.25, add .scrollUp
-	if (currentScroll > previousScroll && currentScroll > navHeight * 2.25) {
-		$(navbar).addClass('scrollUp');
-		//When scrolling up AND you've scrolled less than navHeight, remove .scrollUp
-	} else if (previousScroll > currentScroll && !(currentScroll <= navHeight)) {
-		$(navbar).removeClass('scrollUp');
-	}
-	previousScroll = currentScroll;
-});
+		  if ($(window).scrollTop() > distance) {
+			$('nav').css("background", "rgb(0,0,0,0.9)");
+			}
+			else {
+				$('nav').css("background", "rgb(0,0,0,0)");
+			}
 
-/* Add navbar background color when it is not collapsed */
-$('#navbarTopCollapsible').on('show.bs.collapse', function () {
-	$('.navbar-top').addClass('bg-dark-trans');
-});
-$('#navbarTopCollapsible').on('hide.bs.collapse', function () {
-	$('.navbar-top').removeClass('bg-dark-trans');
-});
+	 });
+
+	});
+
+	
